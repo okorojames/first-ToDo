8f994b19dff666a524c8ccb8a49b357209c2101c
okorojames <okorojameschizaram@gmail.com>
2022-12-24 00:43:53 +0100

    ci/cd

--- js/replica.js
+++ js/replica.js
@@ -9,7 +9,21 @@ date.textContent = fullDate;
 const addContent = document.querySelector(".todoThirdContent");
 const delBtn = document.querySelector(".todo-delete");
 const addBtn = document.querySelector(".btn");
-const userInput = document.querySelector(".todoHere").value;
+
 addBtn.addEventListener("click", addToDo);
 function addToDo() {
+  const userInput = document.querySelector(".todoHere").value;
+  if (userInput === "" || userInput === " " || userInput === "  ") {
+    alert("please enter something");
+  } else {
+    addContent.innerHTML += `
+    
+<div class="todo-item">
+            <p class="todo-paragraph">${userInput}</p>
+            <i class="fas fa-trash todo-delete"></i>
+          </div>
+    
+    `;
+    userInput.value = ``;
+  }
 }
